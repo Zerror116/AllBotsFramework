0c2ee2165478aa7a3a5ccc2f04b30ef768d2b9c5
Zerror116 <zerotwo02166@gmail.com>
2026-01-05 23:28:20 +0400

    Что-то странное в db.py

--- db/db.py
+++ db/db.py
@@ -1,24 +1,29 @@
 import json
 import os
+import traceback
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import as_declarative
 
-path_to_config = os.environ.get("PATH_TO_CONFIG", "config.json")
+try:
+    path_to_config = os.environ.get("PATH_TO_CONFIG", "config.json")
 
-with open(path_to_config, 'r') as f:
-    config = json.load(f)
+    with open(path_to_config, 'r') as f:
+        config = json.load(f)
 
-db_user = config["database"]["username"]
-db_host = config["database"]["host"]
-db_password = config["database"]["password"]
-db_database = config["database"]["name"]
+    db_user = config["database"]["username"]
+    db_host = config["database"]["host"]
+    db_password = config["database"]["password"]
+    db_database = config["database"]["name"]
 
-engine = create_engine(f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}/{db_database}")
+    engine = create_engine(
+        f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}/{db_database}"
+    )
 
+    @as_declarative()
+    class AbstractModel:
+        pass
 
-
-@as_declarative()
-class AbstractModel: pass
-
-
+except Exception:
+    print("Ошибка при инициализации базы данных:")
+    print(traceback.format_exc())
